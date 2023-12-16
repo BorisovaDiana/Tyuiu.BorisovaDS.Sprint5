fe8166887af1cda798b8f18ea684365371db3a83
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-12-16 05:16:06 +0300

    Task3

--- Tyuiu.BorisovaDS.Sprint5.Task3.V6.Test/DataServiceTest.cs
+++ Tyuiu.BorisovaDS.Sprint5.Task3.V6.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.BorisovaDS.Sprint5.Task3.V6.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Tatur\source\repos\Tyuiu.BorisovaDS.Sprint5\Tyuiu.BorisovaDS.Sprint5.Task3.V6\bin\Debug\OutPutFileTask3.bin";
+            string path = @"C:\Users\User\source\repos\Tyuiu.BorisovaDS.Sprint5\Tyuiu.BorisovaDS.Sprint5.Task3.V6\bin\Debug\OutPutFileTask3.bin";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
